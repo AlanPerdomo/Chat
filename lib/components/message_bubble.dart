6b1965dd395345342f6685d0511f6d4bff40988c
Alan Perdomo <alanperdomo@hotmail.com>
2024-07-23 12:49:11 -0300

    #383

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -11,46 +11,79 @@ class MessageBubble extends StatelessWidget {
     required this.belongsToCurrentUser,
   });
 
+  Widget _showUserImage(String imageUrl) {
+    ImageProvider? provider;
+
+    final uri = Uri.parse(imageUrl);
+    if (uri.isAbsolute) {
+      provider = NetworkImage(imageUrl);
+    } else {
+      provider = AssetImage(imageUrl);
+    }
+
+    return CircleAvatar(
+      backgroundImage: provider,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: belongsToCurrentUser
-          ? MainAxisAlignment.end
-          : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: belongsToCurrentUser
-                ? Colors.grey[300]
-                : Theme.of(context).primaryColor,
-            borderRadius: const BorderRadius.all(
-              Radius.circular(8),
+        Row(
+          mainAxisAlignment: belongsToCurrentUser
+              ? MainAxisAlignment.end
+              : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: belongsToCurrentUser
+                    ? Colors.grey[300]
+                    : Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(10),
+                  topRight: const Radius.circular(10),
+                  bottomLeft: belongsToCurrentUser
+                      ? const Radius.circular(10)
+                      : Radius.zero,
+                  bottomRight: belongsToCurrentUser
+                      ? Radius.zero
+                      : const Radius.circular(10),
+                ),
+              ),
+              width: 180,
+              padding: const EdgeInsets.all(16),
+              margin: const EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: 16,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    message.userName,
+                    style: TextStyle(
+                      color: belongsToCurrentUser ? Colors.black : Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    message.text,
+                    style: TextStyle(
+                      color: belongsToCurrentUser ? Colors.black : Colors.white,
+                    ),
+                    textAlign:
+                        belongsToCurrentUser ? TextAlign.end : TextAlign.start,
+                  ),
+                ],
+              ),
             ),
-          ),
-          padding: const EdgeInsets.all(16),
-          margin: const EdgeInsets.symmetric(
-            vertical: 10,
-            horizontal: 16,
-          ),
-          child: Column(
-            children: [
-              Text(
-                message.userName,
-                style: TextStyle(
-                  color: belongsToCurrentUser ? Colors.black : Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                message.text,
-                style: TextStyle(
-                  color: belongsToCurrentUser ? Colors.black : Colors.white,
-                ),
-                textAlign:
-                    belongsToCurrentUser ? TextAlign.end : TextAlign.start,
-              ),
-            ],
-          ),
+          ],
+        ),
+        Positioned(
+          top: 0,
+          left: belongsToCurrentUser ? null : 165,
+          right: belongsToCurrentUser ? 165 : null,
+          child: _showUserImage(message.userImageUrl),
         ),
       ],
     );
