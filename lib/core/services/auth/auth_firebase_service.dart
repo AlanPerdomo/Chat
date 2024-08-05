b815b5b27754047308ed3dfa49fe3071125fa5be
Alan Perdomo <alanperdomo@hotmail.com>
2024-08-05 12:22:49 -0300

    #408

--- lib/core/services/auth/auth_firebase_service.dart
+++ lib/core/services/auth/auth_firebase_service.dart
@@ -5,7 +5,6 @@ import 'package:chat/core/services/auth/auth_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/material.dart';
 
 class AuthFirebaseService implements AuthService {
   static ChatUser? _currentUser;
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -36,6 +36,7 @@ class _AuthPageState extends State<AuthPage> {
     } catch (error) {
       // Tratar erro!
     } finally {
+      // ignore: control_flow_in_finally
       if (!mounted) return;
       setState(() => _isLoading = false);
     }
